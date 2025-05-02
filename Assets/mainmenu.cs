b7bdf61a690ebd67a76ea4bde373546710647ef9
fervour23 <152808425+fervour23@users.noreply.github.com>
2025-05-02 22:01:21 +0530

    Initial commit

---
+++ Assets/mainmenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+    public void PlayGame(){
+        SceneManager.LoadScene(1);
+    }
+    public void Quit(){
+        Application.Quit();
+    }
+}
